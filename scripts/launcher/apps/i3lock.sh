8cd33e62d226c5e51397bc59d72b944a63f7bd56
Kalen Michael <kalenmike@gmail.com>
2026-04-13 22:00:40 +0200

    fix: make locktheme absolute

--- scripts/launcher/apps/i3lock.sh
+++ scripts/launcher/apps/i3lock.sh
@@ -2,7 +2,7 @@
 # Script that fires on locking screen
 
 # Define THEME
-source ./locktheme-astro-v2.sh
+source $HOME/Projects/linux/scripts/launcher/apps/locktheme-astro-v2.sh
 
 # Set flags
 IS_DESKTOP=1 # Set to 1 for desktop, 0 for laptop (default)
@@ -18,33 +18,6 @@ BG_4K_IMAGE="$HOME/Pictures/backgrounds/lockscreen/astronaught.png"
 # Assuming DPI of 144 would result in scaling of 1.5x (Standard DPI = 96)
 # 144 / 96= 1.5
 # So you need to divide your coordinates by the scaling factor
-
-# --- Lockscreen Themes ---
-ASTRO_V2=(
-    --radius=38
-    --ring-width=5
-    --ind-pos=x+1377:y+1249
-    --image=$HOME/Pictures/backgrounds/lockscreens/astro-v2.png
-    --ring-color=ffffffff
-    --ignore-empty-password
-    --show-failed-attempts
-    --screen=1
-    --custom-key-commands
-    --cmd-power-sleep="systemctl suspend"
-    --line-color=00000000
-    --ring-color=00000000
-    --inside-color=00000000
-    --insidever-color=00000000
-    --insidewrong-color=00000000
-    --ringver-color=C6A0F6FF
-    --ringwrong-color=ED8796FF
-    --verif-text=""
-    --verif-color=00000000
-    --wrong-text=""
-    --wrong-color=00000000
-    --keyhl-color=A6DA95FF
-    --bshl-color=EED49FFF
-)
 
 reset_ssh_keys() {
     # Remove SSH Keys in memory
